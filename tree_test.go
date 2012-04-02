0946248a927b795b86067a6e09d65ecf20cb5cb8
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-02 04:35:49 -0700

    Tree test

---
+++ tree_test.go
@@ -0,0 +1,22 @@
+package libxml
+
+import "testing"
+import "strings"
+import "fmt"
+
+func recurseTree(depth int, node Node) (found bool) {
+	for n := range node.Children() {
+		found = true
+		fmt.Println(strings.Repeat("  ", depth), n.Name())
+		if recurseTree(depth+1, n) {
+			fmt.Println(strings.Repeat("  ", depth), "/" + n.Name())
+		}
+	}
+	return
+}
+
+func TestTree(t *testing.T) {
+	doc := NewXmlFromString("<html><head><title></title></head><body><div><span></span></div><div><span></span></div></body></html>")
+	recurseTree(1, doc)
+	doc.Free()
+}
